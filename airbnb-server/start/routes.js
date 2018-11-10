8ae346e254bfce8a69fdf0d0892d45ae3b34d324
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-11-10 16:27:50 -0300

    feat: auth and register an user with JWT mode

--- airbnb-server/start/routes.js
+++ airbnb-server/start/routes.js
@@ -16,6 +16,5 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
