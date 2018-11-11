a25d8d33844eca08223f6a8aef98bb66c1650651
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-11-11 01:48:44 -0300

    Property resources

--- airbnb-server/start/routes.js
+++ airbnb-server/start/routes.js
@@ -18,3 +18,12 @@ const Route = use('Route')
 
 Route.post('/users', 'UserController.create')
 Route.post('/sessions', 'SessionController.create')
+
+Route.post('properties/:id/images', 'ImageController.store')
+  .middleware('auth')
+
+Route.get('images/:path', 'ImageController.show')
+
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth')
