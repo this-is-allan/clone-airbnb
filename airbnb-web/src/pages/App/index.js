09ed64c82ae7424eb823c12939925fe7121bfdf5
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-11-14 10:13:44 -0300

    feat: Add property

--- airbnb-web/src/pages/App/index.js
+++ airbnb-web/src/pages/App/index.js
@@ -1,8 +1,10 @@
 import React, { Component, Fragment } from "react";
 
 import Dimensions from "react-dimensions";
-import { Container, ButtonContainer } from "./styles";
+import { Container, ButtonContainer, PointReference } from "./styles";
 
+import { ModalRoute } from "react-router-modal";
+import AddProperty from "../AddProperty";
 import MapGL from "react-map-gl";
 import PropTypes from "prop-types";
 import debounce from "lodash/debounce";
@@ -37,9 +39,10 @@ class Map extends Component {
       longitude: -49.6446024,
       zoom: 12.8,
       bearing: 0,
-      pitch: 0
+      pitch: 0,
     },
-    properties: []
+    properties: [],
+    addActivate: false
   };
 
   componentDidMount() {
@@ -68,9 +71,25 @@ class Map extends Component {
     this.props.history.push("/");
   };
 
+  handleAddProperty = () => {
+    const { match, history } = this.props;
+    const { latitude, longitude } = this.state.viewport;
+    history.push(
+      `${match.url}/properties/add?latitude=${latitude}&longitude=${longitude}`
+    );
+
+    this.setState({ addActivate: false });
+  };
+
   renderActions() {
     return (
       <ButtonContainer>
+        <Button
+          color="#fc6963"
+          onClick={() => this.setState({ addActivate: true })}
+        >
+          <i className="fa fa-plus" />
+        </Button>
         <Button color="#222" onClick={this.handleLogout}>
           <i className="fa fa-times" />
         </Button>
@@ -78,9 +97,30 @@ class Map extends Component {
     );
   }
 
+  renderButtonAdd() {
+    return (
+      this.state.addActivate && (
+        <PointReference>
+          <i className="fa fa-map-marker" />
+          <div>
+            <button onClick={this.handleAddProperty} type="button">
+              Adicionar
+            </button>
+            <button
+              onClick={() => this.setState({ addActivate: false })}
+              className="cancel"
+            >
+              Cancelar
+            </button>
+          </div>
+        </PointReference>
+      )
+    );
+  }
+
   render() {
-    const { containerWidth: width, containerHeight: height } = this.props;
-    const { properties } = this.state;
+    const { containerWidth: width, containerHeight: height, match } = this.props;
+    const { properties, addActivate } = this.state;
     return (
       <Fragment>
         <MapGL
@@ -95,6 +135,12 @@ class Map extends Component {
           <Properties properties={properties} />
         </MapGL>
         {this.renderActions()}
+        {this.renderButtonAdd()}
+        <ModalRoute
+          path={`${match.url}/properties/add`}
+          parentPath={match.url}
+          component={AddProperty}
+        />
       </Fragment>
     );
   }
